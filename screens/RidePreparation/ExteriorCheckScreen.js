7de21cf8ed4dab56c69b80f5b6dd90dc98bc8382
Nicolas Schönborn <nicolas.schoenborn@gmail.com>
2018-11-27 16:57:27 +0100

    Add checklist to exterior check screen

--- screens/RidePreparation/ExteriorCheckScreen.js
+++ screens/RidePreparation/ExteriorCheckScreen.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { CheckBox, Icon } from 'react-native-elements';
 import {
   StyleSheet,
   View,
+  Text,
   Button,
+  Alert,
 } from 'react-native';
 
 class ExteriorCheckScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Exterior Check',
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: 'Exterior Check',
+      headerRight: (
+        <Icon
+          onPress={() => navigation.navigate('Contact')}
+          name="headphones"
+          type="feather"
+          containerStyle={{
+            paddingRight: 10,
+          }}
+        />
+      ),
+    };
   };
 
   render() {
@@ -16,21 +31,64 @@ class ExteriorCheckScreen extends React.Component {
     return (
       <View style={styles.container}>
         <Button
-          onPress={() => navigation.navigate('InteriorCheck')}
-          title="InteriorCheck"
-          color="blue"
-        />
-        <Button
-          onPress={() => navigation.navigate('Contact')}
-          title="Contact"
-          color="blue"
-        />
-        <Button
           onPress={() => navigation.navigate('Map', {
             disableArrivalButton: true,
           })}
           title="Map"
           color="blue"
+        />
+        <Text style={styles.guideText}>
+          Follow the check list and track new issues if required
+        </Text>
+        <CheckBox
+          title="Check Rear Side"
+          checked={false}
+        />
+        <CheckBox
+          title="Check Driver Side"
+          checked={false}
+        />
+        <CheckBox
+          title="Check Driver Side Tires"
+          checked={false}
+        />
+        <CheckBox
+          title="Check Front Side"
+          checked={false}
+        />
+        <CheckBox
+          title={(
+            <View>
+              <Text style={styles.checkBoxLabel}>
+                Check Co-Driver Side
+              </Text>
+              <Text style={{ paddingLeft: 2, }}>
+                {'\u2022'}
+                Co-Driver door: Big Dent
+              </Text>
+            </View>
+          )}
+          checked={false}
+        />
+        <CheckBox
+          title="Check Co-Driver Side Tires"
+          checked={false}
+        />
+
+        <Button
+          onPress={() => Alert.alert(
+            // 'Alert Title',
+            'Confirmation Request',
+            'Confirm that you checked the exterior readiness of the '
+            + 'car according to your besten Wissen and Gewissens',
+            [
+              { text: 'Cancel', style: 'cancel' },
+              { text: 'OK', onPress: () => navigation.navigate('InteriorCheck') },
+            ],
+            { cancelable: true },
+          )}
+          title="Open car"
+          color="green"
         />
       </View>
     );
@@ -41,6 +99,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  guideText: {
+    padding: 20,
+    fontSize: 16,
+  },
+  checkBoxLabel: {
+
   },
 });
 
