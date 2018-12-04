26525ec24958734b39460cc1da768a8e132fab90
Santhos Baala RS <rssanthosbaala@gmail.com>
2018-12-04 19:32:24 +0100

    Tweak login screen

---
+++ store/NextShift.js
@@ -0,0 +1,28 @@
+import { observable } from "mobx";
+import api from "../helpers/api";
+
+const TWO_MINUTES = 2 * 60 * 1000;
+
+export default class NextShiftStore {
+    @observable shift = null;
+    @observable loading = true;
+    @observable error = '';
+
+    lastLoaded = null;
+
+    async load() {
+        if (+new Date() - this.lastLoaded >= TWO_MINUTES) {
+            this.loading = true;
+            try {
+                const response = await api.get('/shifts/next');
+                console.log('---> ', response.data);
+            } catch (e) {
+                this.error = 'Error';
+                console.log(e);
+            } finally {
+                this.loading = false;
+                this.lastLoaded = new Date();
+            }
+        }
+    }
+}
