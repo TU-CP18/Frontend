ea2c78f70a13cc05cac4e18418d6d2ab5bc48920
Clemens Peters <mail@clemenspeters.de>
2019-02-02 14:23:49 +0100

    #26: Adds query headers to support 'Content-Type': 'application/json' in post requests.

--- helpers/api.js
+++ helpers/api.js
@@ -4,7 +4,7 @@ import fakeApi from './fakeApi';
 
 const BASE_URL = Api.host;
 
-const request = (method, url, bodyParams, queryParams) => {
+const request = (method, url, bodyParams, queryParams, queryHeaders) => {
   if (global.devSettings.settings.get('fakeApi')) {
     const response = fakeApi(method, url, bodyParams, queryParams);
     if (response) return response;
@@ -28,18 +28,24 @@ const request = (method, url, bodyParams, queryParams) => {
     options.params = queryParams;
   }
 
+  if (queryHeaders) {
+    options.headers = queryHeaders;
+  }
+
   if (global.userStore.authenticated) {
     // all api endpoints expect the authToken to be transferred in the authorization header
     options.headers.Authorization = `Bearer ${global.userStore.authToken}`;
   }
 
+  console.log('options', options);
+
   return axios(options);
 };
 
-const get = (url, queryParams) => request('get', url, null, queryParams);
-const post = (url, bodyParams, queryParams) => request('post', url, bodyParams, queryParams);
-const put = (url, bodyParams, queryParams) => request('put', url, bodyParams, queryParams);
-const patch = (url, bodyParams, queryParams) => request('patch', url, bodyParams, queryParams);
+const get = (url, queryParams) => request('get', url, null, queryParams, {});
+const post = (url, bodyParams, queryParams, queryHeaders) => request('post', url, bodyParams, queryParams, queryHeaders);
+const put = (url, bodyParams, queryParams) => request('put', url, bodyParams, queryParams, {});
+const patch = (url, bodyParams, queryParams) => request('patch', url, bodyParams, queryParams, {});
 
 export default {
   get, post, put, patch,
